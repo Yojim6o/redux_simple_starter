808f9f6190fd4f092bb237c769be3fb139601422
Jimmy <jgs2017@gmail.com>
2016-02-29 14:15:14 -0600

    complete 1-intro to react

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyDp5YooPHcCI_gG0Usmi8Y4_DdomzKokr0';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// create a new component.  this component should produce some html
+
+const App = () => { // 1. create a factory // 2. create a virtual dom element
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
+}
+
+// take this component's html and put it on the page(in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+				//3. call and create actual DOM element   &   4. put it in a specific location
