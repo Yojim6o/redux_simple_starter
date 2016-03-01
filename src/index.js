acc3c28e7133c12c92ad3a7ef6bcf0a5e5d282c7
Jimmy <jgs2017@gmail.com>
2016-03-01 08:53:52 -0600

    complete section 2-3 youtube api

--- src/index.js
+++ src/index.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDp5YooPHcCI_gG0Usmi8Y4_DdomzKokr0';
 
-// create a new component.  this component should produce some html
+class App extends Component {
+	constructor(props) {
+		super(props);
 
-const App = () => { // 1. create a factory // 2. create a virtual dom element
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch('surfboards');
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
+
+	render() {
+		const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300);
+
+		return (
+			<div>
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+					videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
-// take this component's html and put it on the page(in the DOM)
-
 ReactDOM.render(<App />, document.querySelector('.container'));
-				//3. call and create actual DOM element   &   4. put it in a specific location
